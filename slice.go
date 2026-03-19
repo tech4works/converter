e3974e06896c5dc7c7983ce05641ae787d2844d2
Gabriel Cataldo <gabriel.cataldo@petzpartner.com.br>
2026-03-19 14:32:16 -0300

    feat(slice): add isNonNil utility to enhance nil-check logic in ToSliceIfNonNil

--- slice.go
+++ slice.go
@@ -1,5 +1,7 @@
 package converter
 
+import "reflect"
+
 func ToSlice[T any](v ...T) []T {
 	return v
 }
@@ -7,9 +9,22 @@ func ToSlice[T any](v ...T) []T {
 func ToSliceIfNonNil[T any](v ...T) []T {
 	var ts []T
 	for _, t := range v {
-		if t != nil {
+		if isNonNil(t) {
 			ts = append(ts, t)
 		}
 	}
 	return ts
+}
+
+func isNonNil(i any) bool {
+	if i == nil {
+		return true
+	}
+	val := reflect.ValueOf(i)
+	switch val.Kind() {
+	case reflect.Ptr, reflect.Interface, reflect.Slice, reflect.Map, reflect.Func, reflect.Chan:
+		return !val.IsNil()
+	default:
+		return true
+	}
 }
